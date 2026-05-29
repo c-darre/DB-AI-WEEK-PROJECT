df2cfe1ec61769e21fd09baca89d13f53e000d42
Benoit GILLIOT <166861063+benoit-korben@users.noreply.github.com>
2026-05-29 17:32:43 +0200

    hotfix on rubyllm config

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -2,5 +2,7 @@ RubyLLM.configure do |config|
   config.default_model = 'gpt-4o-mini'
 
   config.openai_api_key = ENV['GITHUB_TOKEN']
+  config.openai_api_base = "https://models.inference.ai.azure.com"
 
+  config.openrouter_api_key = ENV['OPENROUTER_API_KEY']
 end
